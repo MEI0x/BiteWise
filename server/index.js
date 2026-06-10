6446c36652ed0333639d49238e5e1947df8876b4
MEI0x <mustafaemreislak@gmail.com>
2026-06-10 22:42:25 +0300

    Deployment: Remove duplicate cors declaration in server file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
+
 const pool = require('./db'); // Import our DB connection
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
