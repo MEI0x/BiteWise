6dcb8135584c44ce186ee39b7b4a7bd803542d05
MEI0x <mustafaemreislak@gmail.com>
2026-06-10 23:08:40 +0300

    Deployment: Fix pool export syntax alignment between index and db modules

--- server/index.js
+++ server/index.js
@@ -6,7 +6,8 @@ import express from 'express';
 import cors from 'cors';       
 import jwt from 'jsonwebtoken'; 
 import bcrypt from 'bcrypt';
-import pool from './db.js';
+import { pool } from './db.js'; 
+
 
 
 
